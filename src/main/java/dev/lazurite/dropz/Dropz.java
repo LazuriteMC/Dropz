e0ac8fbd6e263086dcf13ed50da7915880d3ae72
Ethan Johnson <ethan@ejohnsons.com>
2021-01-23 12:04:22 -0600

    Got rid of all access wideners :triumph:

--- src/main/java/dev/lazurite/dropz/Dropz.java
+++ src/main/java/dev/lazurite/dropz/Dropz.java
@@ -1,5 +1,6 @@
 package dev.lazurite.dropz;
 
+import dev.lazurite.dropz.access.ItemEntityAccess;
 import dev.lazurite.rayon.api.event.EntityBodyCollisionEvent;
 import dev.lazurite.rayon.api.registry.DynamicEntityRegistry;
 import dev.lazurite.rayon.api.shape.provider.BoundingBoxShapeProvider;
@@ -21,14 +22,7 @@ public class Dropz implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Yeet.");
-
 		DynamicEntityRegistry.INSTANCE.register(ItemEntity.class, BoundingBoxShapeProvider::get, 2.5f);
-		EntityBodyCollisionEvent.ENTITY_COLLISION.register((body1, body2) -> {
-			if (!body1.getEntity().getEntityWorld().isClient()) {
-				if (body1.getEntity() instanceof ItemEntity && body2.getEntity() instanceof ItemEntity) {
-					((ItemEntity) body1.getEntity()).tryMerge(((ItemEntity) body2.getEntity()));
-				}
-			}
-		});
+		EntityBodyCollisionEvent.ENTITY_COLLISION.register(ItemEntityAccess::onCollision);
 	}
 }
--- src/main/java/dev/lazurite/dropz/access/ItemEntityAccess.java
+++ src/main/java/dev/lazurite/dropz/access/ItemEntityAccess.java
@@ -1,11 +1,22 @@
 package dev.lazurite.dropz.access;
 
 import dev.lazurite.dropz.mixin.common.ItemEntityMixin;
+import dev.lazurite.rayon.physics.body.EntityRigidBody;
+import net.minecraft.entity.ItemEntity;
 
 /**
  * This interface is what allows access to the
  * following methods in {@link ItemEntityMixin}.
  */
 public interface ItemEntityAccess {
+    static void onCollision(EntityRigidBody body1, EntityRigidBody body2) {
+        if (!body1.getEntity().getEntityWorld().isClient()) {
+            if (body1.getEntity() instanceof ItemEntity && body2.getEntity() instanceof ItemEntity) {
+                ((ItemEntityAccess) body1.getEntity()).tryMerge(((ItemEntity) body2.getEntity()));
+            }
+        }
+    }
+
     boolean isBlock();
+    void tryMerge(ItemEntity other);
 }
